e4691700b53cce87fb23eb217cdf03e6c26a9388
Dheeraj singh <Dheeraj.2.singh@coforge.com>
2021-12-10 17:07:56 +0530

    adding two rows

--- src/app/emp.service.ts
+++ src/app/emp.service.ts
@@ -10,7 +10,9 @@ import { AddEmployeeComponent } from './add-employee/add-employee.component';
 export class EmpService {
 
    baseurl:string="http://localhost:8080/employee";
-  employees:Employee[]=[{id :1,firstname:"dheeraj",lastname:"singh", email:"dheeraj@gmail.com"}];
+  employees:Employee[]=[{id :1,firstname:"dheeraj",lastname:"singh", email:"dheeraj@gmail.com"},
+{id :2,firstname:"jay",lastname:"pande", email:"jay@gmail.com"},
+{id :2,firstname:"aman",lastname:"rohilla", email:"aman@gmail.com"}];
       len!:any;
 
  
